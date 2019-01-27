00706ea64d4425011fdcc95f486b49518fafd7bc
Bradley Showen <BShowen@me.com>
2019-01-27 10:36:42 -0500

    Minor change to form workflow in articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,7 @@ class ArticlesController < ApplicationController
         @article = Article.new(article_params)
         @article.save
         flash.notice = "Article '#{@article.title}' has been created."
-        redirect_to article_path(@article)
+        redirect_to articles_path
     end
     
     def destroy 
